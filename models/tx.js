9d3d82c6e40522b513c0fe7523a6c64c415e18bd
Znuff <znuffy@gmail.com>
2018-06-09 17:53:20 +0000

    Added timestamp index to database collection.

--- models/tx.js
+++ models/tx.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
  
 var TxSchema = new Schema({
-  txid: { type: String, unique: true, index: true},
+  txid: { type: String, unique: true, index: true, lowercase: true},
   vin: { type: Array, default: [] },
   vout: { type: Array, default: [] },
   total: { type: Number, default: 0 },
-  timestamp: { type: Number, default: 0 },
+  timestamp: { type: Number, default: 0, index: true},
   blockhash: { type: String },
   blockindex: {type: Number, default: 0},
 }, {id: false});
